e5aeb824eb8dc8f85223713b3d09421361d7f77d
Bruce <bruce@yogainternational.com>
2019-04-04 12:53:42 -0400

    adds preliminary setup for server and client side code

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+app.use(express.urlencoded())
 
+app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/client/index.html`)));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
