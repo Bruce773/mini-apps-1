17abf149deb11fc13cca8bcb1774ace1d34d4dd3
Bruce <bruce@yogainternational.com>
2019-04-04 23:31:14 -0400

    adds an almost complete possible solution

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,12 +3,47 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded())
+app.use(express.urlencoded());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/client/index.html`)));
+app.get('/', (req, res) =>
+  res.sendFile(path.join(`${__dirname}/client/index.html`))
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 app.post('/', (req, res) => {
-  console.log(req.body);
+  // res.send(JSON.parse(req.body.jsonData));
+  var dataString = JSON.stringify(req.body.jsonData)
+    .split('\\')
+    .join(' ')
+    .split('"')
+    .join(' ')
+    .split('{')
+    .join(' ')
+    .split('}')
+    .join(' ')
+    .split('[')
+    .join(' ')
+    .split(']')
+    .join(' ')
+    .split(':')
+    .join(' ');
+  var columns = Object.keys(JSON.parse(req.body.jsonData));
+
+  columns.forEach((column) => {
+    console.log(column);
+    dataString = dataString.split(`${column}`).join(' ');
+  });
+
+  res.render('results', {
+    columns: columns,
+    data: dataString,
+    // data: () => {
+    //   Object.keys(JSON.parse(req.body.jsonData)).forEach((item) => {
+    //     console.log(JSON.parse(req.body.jsonData).item);
+    //   });
+    // },
+  });
 });
