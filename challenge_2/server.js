afb25ebaa65774cc5827a693811512a9fa74dceb
Bruce <bruce@yogainternational.com>
2019-04-06 15:32:58 -0400

    adds code for file picker functionality

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+var upload = multer({ dest: `${path.join(`${__dirname}/uploads/`)}` });
+var fs = require('fs');
+var Promise = require('bluebird');
+var readFile = Promise.promisify(fs.readFile);
 const app = express();
 const port = 3000;
 
@@ -13,53 +18,53 @@ app.get('/', (req, res) =>
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-app.post('/', (req, res) => {
-  // res.send(JSON.parse(req.body.jsonData));
-  var dataString = JSON.stringify(req.body.jsonData)
-    .split('\\')
-    .join(' ')
-    .split('"')
-    .join(' ')
-    .split('{')
-    .join(' ')
-    .split('}')
-    .join(' ')
-    .split('[')
-    .join(' ')
-    .split(']')
-    .join(' ')
-    .split(':')
-    .join(' ');
-  var columns = Object.keys(JSON.parse(req.body.jsonData));
-  var counter = 0;
+app.post('/', upload.single('jsonData'), (req, res) => {
+  readFile(`${path.join(`${__dirname}/uploads/${req.file.filename}`)}`).then(
+    (data) => {
+      console.log('!!!!', JSON.parse(data));
+      var fileContent = JSON.parse(data);
+      var dataString = JSON.stringify(fileContent)
+        .split('\\')
+        .join(' ')
+        .split('"')
+        .join(' ')
+        .split('{')
+        .join(' ')
+        .split('}')
+        .join(' ')
+        .split('[')
+        .join(' ')
+        .split(']')
+        .join(' ')
+        .split(':')
+        .join(' ');
+      var columns = Object.keys(JSON.parse(data));
+      var counter = 0;
 
-  columns.forEach((column) => {
-    dataString = dataString.split(`${column}`).join(' ');
-  });
+      columns.forEach((column) => {
+        dataString = dataString.split(`${column}`).join(' ');
+      });
 
-  dataString = dataString
-    .split(' ')
-    .map((item, index) => {
-      if (item === ',') {
-        counter++;
-        if (counter === columns.length) {
-          counter = 0;
-          return (dataString[index + 1] = '\n');
-        }
-      }
-      return item;
-    })
-    .join('');
+      dataString = dataString
+        .split(' ')
+        .map((item, index) => {
+          if (item === ',') {
+            counter++;
+            if (counter === columns.length) {
+              counter = 0;
+              return (dataString[index + 1] = '\n');
+            }
+          }
+          return item;
+        })
+        .join('');
 
-  console.log(dataString);
+      console.log(dataString);
 
-  res.render('results', {
-    columns: columns,
-    data: dataString,
-    // data: () => {
-    //   Object.keys(JSON.parse(req.body.jsonData)).forEach((item) => {
-    //     console.log(JSON.parse(req.body.jsonData).item);
-    //   });
-    // },
-  });
+      res.render('results', {
+        columns: columns,
+        data: dataString,
+      });
+    }
+  );
 });
