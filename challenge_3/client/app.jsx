ac6a8e5a3f89c44a0f5b37ffcfe09eae8386e08f
Bruce <bruce@yogainternational.com>
2019-04-09 19:31:58 -0400

    adds dependencies, React code, and server side Express code

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,19 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
+var FormOne = (props) => (
+  <div className="form-one">
+    <div className="form-label">Name: </div>
+    <input type="text" />
+  </div>
+);
 
+class App extends React.Component {
+  render() {
+    return FormOne();
+  }
+}
+
+ReactDOM.render(<App />, document.getElementsByClassName('app'));
+
+module.exports = App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
