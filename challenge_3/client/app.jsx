730325c5f5d067de61230af9158073ce7fd11925
Bruce <bruce@yogainternational.com>
2019-04-12 09:21:32 -0400

    updates styling adds middleware to handle parsing post request body improves React logic adds mock  homepage with checkout button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-const ReactDOM = require('react-dom');
-
 var Form1 = (props) => (
-  <div className="form">
+  <div>
     <div className="form-label">First Name: </div>
-    <input name="first-name" type="text" />
+    <input name="firstname" type="text" />
     <div className="form-label">Last Name: </div>
-    <input name="last-name" type="text" />
+    <input name="lastname" type="text" />
     <div className="form-label">Email: </div>
     <input name="email" type="text" />
     <div className="form-label">Password: </div>
@@ -15,7 +12,7 @@ var Form1 = (props) => (
 );
 
 var Form2 = (props) => (
-  <div className="form">
+  <div>
     <div className="form-label">Line #1: </div>
     <input name="line-1" type="text" />
     <div className="form-label">Line #2: </div>
@@ -25,18 +22,18 @@ var Form2 = (props) => (
     <div className="form-label">State: </div>
     <input name="state" type="text" />
     <div className="form-label">Zip Code: </div>
-    <input name="zip-code" type="text" />
+    <input name="zipcode" type="text" />
     <div className="form-label">Phone Number: </div>
     <input name="phone-number" type="text" />
   </div>
 );
 
 var Form3 = (props) => (
-  <div className="form">
+  <div>
     <div className="form-label">Card Number: </div>
     <input name="card-number" type="text" />
-    <div className="form-label">Experation Date: </div>
-    <input name="experation-date" type="text" />
+    <div className="form-label">Expiration Date: </div>
+    <input name="expiration-date" type="text" />
     <div className="form-label">CVV: </div>
     <input name="cvv" type="text" />
     <div className="form-label">Billing Zip Code: </div>
@@ -49,35 +46,93 @@ class App extends React.Component {
     super(props);
     this.state = {
       form: 1,
+      formsVisible: false,
+      hover: true,
     };
     this.forms = {
-      '1': 'Form1',
-      '2': 'Form2',
-      '3': 'Form3',
+      '1': Form1,
+      '2': Form2,
+      '3': Form3,
     };
   }
 
   nextForm() {
-    this.state.form < 4
+    this.state.form <= 2
       ? this.setState((state) => {
           return { form: state.form + 1 };
         })
       : this.setState(() => {
-          return { form: 1 };
+          return { formsVisible: false };
         });
   }
 
+  showForms() {
+    this.setState(() => {
+      return { formsVisible: true, form: 1 };
+    });
+  }
+
+  setHover() {
+    this.setState((state) => {
+      return { hover: !state.hover };
+    });
+  }
+
   render() {
-    const Component = this.forms[this.state];
+    var formsDisplay, homeDisplay;
+    const Component = this.forms[this.state.form];
+    if (this.state.formsVisible === true) {
+      formsDisplay = 'block';
+      homeDisplay = 'none';
+    } else {
+      formsDisplay = 'none';
+      homeDisplay = 'block';
+    }
     return (
       <div>
-        <Component />
-        <button onClick={nextForm}>Next</button>
+        <div style={{ display: homeDisplay }}>
+          <button
+            className="checkout-button"
+            style={{
+              fontSize: '25px',
+              padding: '15px 32px',
+              backgroundColor: this.state.hover ? '#fc8f07' : 'white',
+              color: this.state.hover ? 'white' : '#fc8f07',
+              borderColor: '#fc8f07',
+              borderWidth: 'medium',
+              cursor: 'pointer',
+            }}
+            onMouseEnter={this.setHover.bind(this)}
+            onMouseLeave={this.setHover.bind(this)}
+            onClick={this.showForms.bind(this)}
+          >
+            Checkout
+          </button>
+        </div>
+        <div style={{ display: formsDisplay }}>
+          <form
+            method="post"
+            className="form"
+            action="/"
+            onSubmit={this.nextForm.bind(this)}
+          >
+            <Component />
+            <input
+              type="submit"
+              // onClick={this.nextForm.bind(this)}
+              value="Next"
+              style={{
+                padding: '10px 26px',
+                marginTop: '22px',
+                backgroundColor: 'rgb(127, 123, 119)',
+                color: 'white',
+              }}
+            />
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementsByClassName('app'));
-
-module.exports = App;
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
@@ -11,4 +14,8 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   // res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
 });
